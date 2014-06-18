e76aa882c05a0bf5e4a00b5b6517536cc090df66
Sean Wendt <sean.wendt@chartboost.com>
2014-06-17 17:09:39 -0700

    fixed missing folders
    fixed missing folders in repo

---
+++ iOS/ChartboostExample/ChartboostExampleApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChartboostExampleApp
+//
+//  Created by Gonzalo Alsina on 11/6/13.
+//  Copyright (c) 2013 Chartboost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
